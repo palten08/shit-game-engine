a6ab3082938f106f60740ed45fe09b0733f1f388
Pete <thepetecloud@gmail.com>
2026-02-27 15:59:05 -0500

    Fix the weird compounding transforms thing

--- src/debug.c
+++ src/debug.c
@@ -5,9 +5,14 @@
 
 Scene test_update_scene(Scene *scene) {
     // Matrix used for slightly rotating on every frame
-    Matrix3 rotation_matrix = mat3_create_rotation_matrix(0.01f);
+    //Matrix3 rotation_matrix = mat3_create_rotation_matrix(0.01f);
 
     for (int i = 0; i < scene->square_count; i++) {
+        // Create a rotation matrix for the current square's rotation angle
+        Matrix3 rotation_matrix = mat3_create_rotation_matrix(scene->squares[i].current_rotation_angle);
+        // Increment the square's rotation angle for the next frame
+        scene->squares[i].current_rotation_angle += 0.01f; // Adjust
+
         // Translation matrix used for moving the vertex position to the origin
         Matrix3 to_origin_matrix = mat3_create_translation_matrix(-scene->squares[i].origin.x, -scene->squares[i].origin.y);
 
@@ -17,9 +22,10 @@ Scene test_update_scene(Scene *scene) {
         Matrix3 combined = mat3_multiply(from_origin_matrix, mat3_multiply(rotation_matrix, to_origin_matrix)); // Combine the translation and rotation matrices into a single matrix that I can apply to the vertex position in one step
 
         // Iterate through the square's vertices and apply the combined transformation matrix to each vertex position
+        // We use the originals and not the currents to avoid the weird compounding transform thing
         for (int j = 0; j < 4; j++) {
 
-            Vector3f resultant_vertex_pos = mat3_multiply_vec3(combined, (Vector3f){scene->squares[i].vertices[j].position.x, scene->squares[i].vertices[j].position.y, 1.0f}); // Apply the combined transformation matrix to the vertex position
+            Vector3f resultant_vertex_pos = mat3_multiply_vec3(combined, (Vector3f){scene->squares[i].original_vertices[j].x, scene->squares[i].original_vertices[j].y, 1.0f}); // Apply the combined transformation matrix to the vertex position
 
             // Update the vertex position with the new rotated position
             scene->squares[i].vertices[j].position.x = (int)resultant_vertex_pos.x;
